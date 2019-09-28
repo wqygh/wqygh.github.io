b7f6d782da5146d5285fa1aba9ba4ca54741a167
Qiyuan <qwu312@gatech.edu>
2019-09-28 14:53:29 -0600

    updating publication list

--- script/publication.js
+++ script/publication.js
@@ -1,12 +1,20 @@
 var csv = d3.csv("../asset/data/publication_list.csv")
 
 csv.then(function(data){
-  console.log(data)
+
   var pub_list = d3.select("#pub_list")
-  pub_list.selectAll("a")
+  pub_list.selectAll("p")
           .data(data)
           .enter()
-          .append("a")
-          .attr("href", function (d) {return d.link;})
-          .text(function (d){return d.title;});
+          .append("p")
+          .attr("id", "pub_list_item")
+          .text(function (d, i){return i+1 + ". " + d.title + ", " + d.year+ ", ";});
+
+  var pub_list_item = d3.selectAll("#pub_list_item")
+  pub_list_item.append("a")
+               .data(data)
+               .attr("href", d => d.link)
+               .text(d => d.journal)
+               .attr("class", "pub_item_link")
+               .attr("target", "_blank")
 })

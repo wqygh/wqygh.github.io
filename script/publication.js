f25b1a50d334a31587f6935b7ac0909a54184888
Qiyuan <qwu312@gatech.edu>
2019-09-28 11:32:52 -0600

    fixed typo in head

--- script/publication.js
+++ script/publication.js
@@ -1,11 +1,12 @@
 var csv = d3.csv("../asset/data/publication_list.csv")
 
 csv.then(function(data){
-    var pub_list = d3.select("#pub_list")
-    pub_list.selectAll("a")
-      .enter()
-      .data(data)
-      .append("a")
-      .attr("href", data.link)
-      .text(d.title)
+  console.log(data)
+  var pub_list = d3.select("#pub_list")
+  pub_list.selectAll("a")
+          .data(data)
+          .enter()
+          .append("a")
+          .attr("href", function (d) {return d.link;})
+          .text(function (d){return d.title;});
 })
